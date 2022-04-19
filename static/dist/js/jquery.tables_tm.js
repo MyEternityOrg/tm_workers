c651c2d3f8e63e13993f6b6552ca0d577ca89580
MyEternity <spddarkforce47@gmail.com>
2022-04-19 12:15:54 +0300

    some optimizations

--- static/dist/js/jquery.tables_tm.js
+++ static/dist/js/jquery.tables_tm.js
@@ -1,6 +1,13 @@
 (function ($) {
-	$('.clickable-row').css('cursor', 'pointer');
-	$(".clickable-row").click(function () {
-		window.location = $(this).data("href");
-	});
+    $('.clickable-row').css('cursor', 'pointer');
+    $(".clickable-row").click(function () {
+        window.location = $(this).data("href");
+    });
+})(jQuery);
+
+(function ($) {
+    $('.clickable-row_np').css('cursor', 'pointer');
+    $(".clickable-row_np").click(function () {
+        window.open($(this).data("href"), "_blank");
+    });
 })(jQuery)
