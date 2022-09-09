2c522ecca1e22bf2081e2900a4644c933770964b
MyEternity <spddarkforce47@gmail.com>
2022-09-09 10:28:06 +0300

    some interface optimizations exworkers templates refactor

--- extworkers/urls.py
+++ extworkers/urls.py
@@ -7,8 +7,8 @@ from .views import ShopList, ShopRecord, PersonRecordAdd, PersonRecordModify, Pe
 app_name = 'extworkers'
 
 urlpatterns = [
-                  path('', ShopList.as_view(), name='fill_data'),
-                  path('<pk>/<dts>/', ShopRecord.as_view(), name='fill_data_shop'),
+                  path('', ShopList.as_view(), name='shoplist'),
+                  path('<pk>/<dts>/', ShopRecord.as_view(), name='shopdata'),
                   path('<pk>/<dts>/add/', PersonRecordAdd.as_view(), name='person_add'),
                   path('<dv>/<dts>/edit/<pk>', PersonRecordModify.as_view(), name='person_edit'),
                   path('<dv>/<dts>/delete/<pk>', PersonRecordDelete.as_view(), name='person_delete')
