6aea207907e63beb1679df8982ea028b53ff1e0c
kerleada7 <kerleada7@mail.ru>
2022-09-19 11:09:57 +0300

    old_new

--- reports/urls.py
+++ reports/urls.py
@@ -8,4 +8,6 @@ app_name = 'reports'
 
 urlpatterns = [
                 path('actservices/', report_act_service, name='report_act_service'),
+                path('select_contractor/', select_contractor, name='select_contractor'),
+
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
